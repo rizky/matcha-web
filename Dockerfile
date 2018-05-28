8afc7118eded04655dd262f1552bf4566cd4a3e2
Rizky Ario <rizkyario@yahoo.com>
2018-05-29 00:16:22 +0200

    Add Bootstrap

--- Dockerfile
+++ Dockerfile
@@ -11,10 +11,6 @@ ENV PATH /usr/src/app/node_modules/.bin:$PATH
 # install and cache app dependencies
 COPY package.json /usr/src/app/package.json
 RUN npm install --silent
-RUN npm install react-scripts@1.1.1 -g --silent
-
-# start app
-CMD ["npm", "start"]
 
 # docker build -t rizkyario/42-matcha .
 # docker push rizkyario/42-matcha
--- src/App.js
+++ src/App.js
@@ -1,15 +1,40 @@
 import React, { Component } from 'react';
+import { Navbar, Nav, NavDropdown, MenuItem, NavItem } from 'react-bootstrap';
+import FontAwesome from '@fortawesome/fontawesome'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
+			<Navbar>
+				<Navbar.Header>
+					<Navbar.Brand>
+					<a href="#home">Matcha</a>
+					</Navbar.Brand>
+				</Navbar.Header>
+				<Nav pullRight>
+					<NavDropdown eventKey={3} title="Account">
+						<MenuItem eventKey={3.1}>Profile</MenuItem>
+						<MenuItem eventKey={3.2}>Settings</MenuItem>
+						<MenuItem divider />
+						<MenuItem eventKey={3.4}>Log out</MenuItem>
+					</NavDropdown>
+				</Nav>
+			</Navbar>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+		<footer>
+			<Navbar className="navbar-fixed-bottom">
+				<Nav pullRight>
+					<NavItem eventKey={1} href="#">
+						2018 @ Riz.ky
+					</NavItem>
+				</Nav>
+			</Navbar>
+		</footer>
       </div>
     );
   }
