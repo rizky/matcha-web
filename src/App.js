93e0dc1c861c6023cf3f19ae7b4018404f0d805e
Rizky Ario <rizkyario@yahoo.com>
2018-05-29 20:22:34 +0200

    Fix routing

--- src/App.js
+++ src/App.js
@@ -5,21 +5,22 @@ import * as Pages from './Pages'
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 class App extends Component {
-  render() {
-    return (
-	<Router>
-		<div className="App">
-			<Navigator/>
-				{
-					Object.keys(Pages).map((page, index) => {
-						return <Route key={index} path={'/' + page.toLowerCase()} component={Pages[page]} />
-					})
-				}
-			<Footer/>
-		</div>
-	</Router>
-    );
-  }
+	render() {
+		return (
+		<Router>
+			<div className="App">
+				<Navigator/>
+					{
+						Object.keys(Pages).map((page, index) => {
+							let path = (page.toLowerCase() === 'home') ? '/' : '/' + page.toLowerCase();
+							return <Route key={index} exact path={path} component={Pages[page]} />
+						})
+					}
+				<Footer/>
+			</div>
+		</Router>
+		);
+	}
 }
 
 export default App;
