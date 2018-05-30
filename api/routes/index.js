899a729faa4ae1d3834ee49ecb52e0c73095e98a
Rizky Ario <rizkyario@yahoo.com>
2018-05-30 20:46:50 +0200

    ES6-ed

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,10 +1,12 @@
 import express from 'express'
-import { users, photos } from './controllers'
+import * as Controllers from './controllers'
 
 var router = express.Router()
 
-router.use('/users', users);
-router.use('/photos', photos);
+// Load all controllers
+Object.keys(Controllers).forEach((key) => {
+	router.use(`/${key}`, Controllers[key]);
+})
 
 router.get('/', (req, res, next) => {
 	res.render('index', { title: 'Express' })
