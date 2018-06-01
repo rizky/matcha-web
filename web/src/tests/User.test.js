543267fe9e6912c329755ee8b04b7787f32fdadc
Rizky Ario <rizkyario@yahoo.com>
2018-06-01 15:47:55 +0200

    Setup enzym

--- web/src/tests/User.test.js
+++ web/src/tests/User.test.js
@@ -1,21 +1,22 @@
 import React from 'react';
 import User from '../components/User';
+import { shallow } from 'enzyme';
 import renderer from 'react-test-renderer';
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });
 
 test('User is NULL', () => {
 	let user = null
-	const component = renderer.create(
-			<User user={user} />,
-	);
-	let tree = component.toJSON();
-	expect(tree).toMatchSnapshot();
+	const component = shallow(<User user={user} />);
+	expect(component.type()).toEqual(null);
 });
 
 test('User is valid', () => {
 	let user = {
 		activeAt:"2018-05-30T16:39:15.000Z",
 		createdAt:"2018-05-30T16:39:15.000Z",
-		deleted:{type: "Buffer", data: Array(1)},
 		dob:"1988-05-30T16:39:15.000Z",
 		email:"camagru.rizky@gmail.com",
 		id:"0000000001",
@@ -30,9 +31,14 @@ test('User is valid', () => {
 		updatedAt:null,
 		username:"admin",
 	}
-	const component = renderer.create(
-			<User user={user} />,
-	);
-	let tree = component.toJSON();
-	expect(tree).toMatchSnapshot();
+	const component = shallow(<User user={user} />);
+	expect(component.length).toEqual(1);
+	expect(component.find('.user').text()).toEqual(user.name+', ');
 });
+
+test('Distance calculation', null);
+test('Active time calculation', null);
+test('Show more option', null);
+test('Unmatch', null);
+test('Like', null);
+test('Pass', null);
