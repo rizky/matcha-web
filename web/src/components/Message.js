fc5a1e710c0d20aad2e4db11d0aaac55d313e5a2
rizkyario <rizkyario@yahoo.com>
2018-06-11 22:25:37 +0200

    Fix style

--- web/src/components/Message.js
+++ web/src/components/Message.js
@@ -13,15 +13,18 @@ class Message extends Component {
 	render () {
 		const { message, userContext } = this.props
 		const isYou = message.from.id === userContext.id ? {justifySelf: 'end'} : null
-		const time = this.formatDate(new Date(Date.parse(message.createdAt)))
 		return (
-			<div style={{display: 'grid'}} id="message_{{message.id}}">
-				<span className='time_text'>{time}</span>
+			<div style={{display: 'grid'}} id="message_{{message.id}}">				
+				{
+				message.match.data[0]
+				? <span className='time_text'>{this.formatFullDate(new Date(Date.parse(message.createdAt)))}</span>
+				: <span className='time_text'>{this.formatDate(new Date(Date.parse(message.createdAt)))}</span>
+				}
 				{
 				message.match.data[0]
 				? <div style={{textAlign: 'center', margin: '10px', marginBottom: '0px', width: '100%'}}>
 					<hr style={{marginLeft: '-10px'}}/>
-					<span style={{background: '#fff', position:'relative', top:'-19px', padding: '10px', color: '#b2b2b2'}}>Your are match</span>
+					<span style={{background: '#fff', position:'relative', top:'-19px', left: '-8px', padding: '10px', color: '#b2b2b2'}}>Your are match</span>
 				</div>
 				: <div className='message' style={isYou}>
 					{
@@ -53,7 +56,22 @@ class Message extends Component {
 			return `${day} ${monthNames[monthIndex]} ${year}, ${hour}:${minute}`;
 		else
 			return `${hour}:${minute}`;
-	  }
+	}
+	formatFullDate(date) {
+		var monthNames = [
+		  "January", "February", "March",
+		  "April", "May", "June", "July",
+		  "August", "September", "October",
+		  "November", "December"
+		];
+		var day = date.getDate();
+		var monthIndex = date.getMonth();
+		var year = date.getFullYear();
+	 
+		var hour = date.getHours();
+		var minute = date.getMinutes();
+		return `${day} ${monthNames[monthIndex]} ${year}, ${hour}:${minute}`;
+	}
 }
 
 export default React.forwardRef((props, ref) => (
