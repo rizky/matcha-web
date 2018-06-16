21c11d34a2e58296a53675ea185afa7e085d77b4
rizkyario <rizkyario@yahoo.com>
2018-06-16 18:57:40 +0200

    Fix Thread/match

--- api/routes/models/Thread.js
+++ api/routes/models/Thread.js
@@ -33,6 +33,7 @@ export default class Thread extends ORM {
 			return new Promise (
 				(resolve, reject) => {
 					Thread.insert(params, (err, data) => {
+						params['id'] = data.insertId
 						if (err)
 							reject(err)
 						else
