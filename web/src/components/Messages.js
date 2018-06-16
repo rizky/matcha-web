c710e501b69c0f8e5ffb9dda79a693295f94b183
rizkyario <rizkyario@yahoo.com>
2018-06-16 22:15:27 +0200

    Implement message text behaviour

--- web/src/components/Messages.js
+++ web/src/components/Messages.js
@@ -1,12 +1,37 @@
 import React, { Component } from 'react'
 import { Message } from '../components'
 import './Message.css'
+import { dispatch } from '../index'
+import * as MessageActions from '../redux/actions/message'
 
 export default class Messages extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {message: ''};
+	}
+
+	handleChange = (e) => {
+		this.setState({
+			message: e.target.value
+		})
+	}
+
+	sendMessage = (e) => {
+		if (e.key === 'Enter') {
+			const { selectedThread } = this.props
+
+			this.setState({
+				message: ''
+			})
+			dispatch(MessageActions.loadMessages(selectedThread.id))
+		}
+	}
+
 	render () {
-		if (this.props.messages == null)
+		var { messages } = this.props
+		if (messages == null)
 			return (<div></div>)
-		let messages = this.props.messages.map ( message => {
+		messages = this.props.messages.map ( message => {
 			if (message.from.id !== this.props.userContext.id && message.match.data[0])
 				return null
 			return <Message key={message.id} message={message}/>
@@ -15,10 +40,13 @@ export default class Messages extends Component {
 			<div className='messagesPane'>
 				<div className='messages'>{messages}</div>
 				<div className='messages'>
-				<input placeholder='Write message...' className='message-text'/>
+				<input placeholder='Write message...' 
+					className='message-text' 
+					value={this.state.message} 
+					onChange={this.handleChange} 
+					onKeyPress={this.sendMessage}/>
 				</div>
 			</div>
 		)
-
 	}
 }
