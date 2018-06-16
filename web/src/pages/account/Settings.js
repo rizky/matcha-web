bfe2e31d8bd560eaa6fbd8c60e9ea67072a99ac7
rizkyario <rizkyario@yahoo.com>
2018-06-16 23:33:28 +0200

    Get UserContext at settings

--- web/src/pages/account/Settings.js
+++ web/src/pages/account/Settings.js
@@ -1,34 +1,36 @@
 import React, { Component } from 'react'
+import { UserContext } from '../../App'
 
 class Settings extends Component {
 	render() {
+		const { userContext } = this.props
 		return (
 			<div class="forms">
 			<h1>Settings</h1>
 			<form action='/account/settings' method='POST'>
 				<h2>Personal</h2>
-				<input type='text' name='name' placeholder='Name' value=''/>
+				<input type='text' name='name' placeholder='Name' value={userContext.name}/>
 				<hr />
-				<input type='text' name='username' placeholder='Username' value=''/>
+				<input type='text' name='username' placeholder='Username' value={userContext.username}/>
 				<hr />
-				<input type='email' name='email' placeholder='Email' value=''/>
+				<input type='email' name='email' placeholder='Email' value={userContext.email}/>
 				<hr />
-				<input type='text' name='location' placeholder='Location' value=''/>
+				<input type='text' name='location' placeholder='Location' value={userContext.location}/>
 				<hr />
 				<input type='text' placeholder='Gender' disabled value=''/>
 				<div style={{display: 'flex'}}>
-					<input type='radio' style={{width: '1em', height: '1em'}} name='subscribed' value='true'/>
-					<label for='subscribeNews'>Male</label>
-					<input type='radio' style={{width: '1em', height: '1em'}} name='subscribed' value='true'/>
-					<label for='subscribeNews'>Female</label>
+					<input type='radio' style={{width: '1em', height: '1em'}} name='gender-male'/>
+					<label for='gender'>Male</label>
+					<input type='radio' style={{width: '1em', height: '1em'}} name='gender-female'/>
+					<label for='gender'>Female</label>
 				</div>
 				<hr />
 				<input type='text' placeholder='Preferences' disabled value=''/>
 				<div style={{display: 'flex'}}>
-					<input type='checkbox' style={{width: '1em', height: '1em'}} name='subscribed' value='true'/>
-					<label for='subscribeNews'>Male</label>
-					<input type='checkbox' style={{width: '1em', height: '1em'}} name='subscribed' value='true'/>
-					<label for='subscribeNews'>Female</label>
+					<input type='checkbox' style={{width: '1em', height: '1em'}} name='preferences-male'/>
+					<label for='preferences'>Male</label>
+					<input type='checkbox' style={{width: '1em', height: '1em'}} name='preferences-female'/>
+					<label for='preferences'>Female</label>
 				</div>
 				<hr />
 				<br />
@@ -48,4 +50,8 @@ class Settings extends Component {
  	}
 }
 
-export default Settings
+export default React.forwardRef((props, ref) => (
+	<UserContext.Consumer>
+		{user => <Settings {...props} userContext={user} ref={ref}/>}
+	</UserContext.Consumer>
+));
